15c62c05639aa658e402c87d7461a0ad8fc23c59
yoyoNTNU <yoyo102102102@gmail.com>
2023-07-03 18:32:14 +0800

    fix:rename type to type_

--- backend/db/migrate/20230702091140_create_messages.rb
+++ backend/db/migrate/20230702091140_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration[7.0]
     create_table :messages do |t|
       t.belongs_to :chatroom , null: false, foreign_key: true
       t.belongs_to :member , null: false, foreign_key: true
-      t.string :type #string or photo
+      t.string :type_ #string or photo
       t.text :content
       t.string :photo
       t.boolean :isPinned
