42a2828cce720d89ec3cbf2e6ac9eaf91c646111
yoyoNTNU <yoyo102102102@gmail.com>
2023-07-11 00:23:06 +0800

    feat:member info RU

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
     mount_devise_token_auth_for 'Member', at: 'auth/member'
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    namespace :api do
+        get 'member/info'=> 'member#show'
+        patch 'member/info' => 'member#update'
 
+    end
     # Defines the root path route ("/")
     # root "articles#index"
 end
