074e2567d002df1b335f470a7bc32d3bb0d3146a
yoyoNTNU <yoyo102102102@gmail.com>
2023-07-10 15:08:38 +0800

    fix:some set

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'Member', at: 'auth'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    mount_devise_token_auth_for 'Member', at: 'auth/member'
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # Defines the root path route ("/")
+    # root "articles#index"
 end
