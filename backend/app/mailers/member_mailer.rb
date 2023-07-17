7e8e82cf8876b2d70ec58cab0de253da70edf8dd
yoyoNTNU <yoyo102102102@gmail.com>
2023-07-17 13:46:42 +0800

    fix:login fail with error email can't find nil.email method

--- backend/app/mailers/member_mailer.rb
+++ backend/app/mailers/member_mailer.rb
@@ -6,6 +6,8 @@ class MemberMailer < ApplicationMailer
 
   def login_failed_email(user)
     @resource = user
-    mail(to: user.email, subject: '【Express Message】登入失敗通知')
+    if user
+      mail(to: user.email, subject: '【Express Message】登入失敗通知')
+    end
   end
 end
