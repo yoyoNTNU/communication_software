4f18cef1ac866e1f4d1db081aa3c800ce35424c3
yoyoNTNU <yoyo102102102@gmail.com>
2023-07-17 12:57:55 +0800

    fix:authenticate_member! render value format

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,9 +3,20 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_id, :photo, :background, :birthday, :introduction,:email ,:password ,:password_confirmation, :name, :phone])
+  end
+
+  def authenticate_member!
+    unless member_signed_in?
+      render :json => {
+        error: true,
+        message: "Unauthorized",
+        data: "The user is not login."
+      }.to_json, :status => 401
+    end
   end
 end
