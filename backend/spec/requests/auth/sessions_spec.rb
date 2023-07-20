eec29a31517aa483e3bce0f9cc60b146e34a103f
weihsiang120 <weihsiang120@gmail.com>
2023-07-20 13:58:08 +0800

    rspec: add sign out example

--- backend/spec/requests/auth/sessions_spec.rb
+++ backend/spec/requests/auth/sessions_spec.rb
@@ -59,19 +59,42 @@ RSpec.describe "Auth::Sessions", type: :request do
   end
 end
 
-example "failed to sign in with uncertified email" do
-  m=Member.new(user_id:"test",name:"test",phone:"0912345678",email:"example@email.com",password:"Example123", is_login_mail: false)
-  m.save
-  post "/auth/member/sign_in",
-  params:{email: "example@email.com", password:"Example123"}
-  expect(response).to have_http_status(401)
-  expect(JSON.parse(response.body)).to eq(
-    JSON.parse(
-      {
-        "error": true,
-        "message": "failed to sign in",
-        "data": "A confirmation email was sent to your account at 'example@email.com'. You must follow the instructions in the email before your account can be activated"
-      }.to_json
+describe "POST /auth/member/sign_in" do
+  example "failed to sign in with uncertified email" do
+    m=Member.new(user_id:"test",name:"test",phone:"0912345678",email:"example@email.com",password:"Example123", is_login_mail: false)
+    m.save
+    post "/auth/member/sign_in",
+    params:{email: "example@email.com", password:"Example123"}
+    expect(response).to have_http_status(401)
+    expect(JSON.parse(response.body)).to eq(
+      JSON.parse(
+        {
+          "error": true,
+          "message": "failed to sign in",
+          "data": "A confirmation email was sent to your account at 'example@email.com'. You must follow the instructions in the email before your account can be activated"
+        }.to_json
+      )
     )
-  )
+  end
+end
+
+describe "DELETE /auth/member/sign_out" do
+  example "succeed to sign out" do
+    m=Member.new(user_id:"test",name:"test",phone:"0912345678",email:"example@email.com",password:"Example123")
+    m.skip_confirmation!
+    m.save
+    post "/auth/member/sign_in",params:{email:"example@email.com",password:"Example123"}
+    @header={Authorization: response.headers["Authorization"]} 
+    delete "/auth/member/sign_out", headers:@header
+    expect(response).to have_http_status(200)
+    expect(JSON.parse(response.body)).to eq(
+      JSON.parse(
+        {
+          "error": false,
+          "message": "succeed to sign out",
+          "data": {}
+        }.to_json
+      )
+    )
+  end
 end
