e36728bb78eeff39bcb16ed6411ed4412a77f586
weihsiang120 <weihsiang120@gmail.com>
2023-07-20 12:12:53 +0800

    rspec: add failed to sign in tests

--- backend/spec/requests/auth/sessions_spec.rb
+++ backend/spec/requests/auth/sessions_spec.rb
@@ -27,9 +27,9 @@ RSpec.describe "Api::Members", type: :request do
       expect(parsed_body["message"]).to eq("succeed to sign in")
     end
     
-    example "failed to sign in" do 
+    example "failed to sign in with incorrect password" do 
       post "/auth/member/sign_in",
-      params:{email:"",password:""}
+      params:{email:"example@email.com",password:""}
       expect(response).to have_http_status(401)
       expect(JSON.parse(response.body)).to eq(
         JSON.parse(
@@ -41,5 +41,44 @@ RSpec.describe "Api::Members", type: :request do
         )
       )
     end
+
+    example "failed to sign in with incorrect email" do 
+      post "/auth/member/sign_in",
+      params:{email:"",password:"Example123"}
+      expect(response).to have_http_status(401)
+      expect(JSON.parse(response.body)).to eq(
+        JSON.parse(
+          {
+            "error": true,
+            "message": "failed to sign in",
+            "data": "Invalid login credentials. Please try again."
+          }.to_json
+        )
+      )
+    end
+
+  end
+end
+
+RSpec.describe "Api::Members", type: :request do
+  before do
+    m=Member.new(user_id:"test",name:"test",phone:"0912345678",email:"example@email.com",password:"Example123")
+    m.save
+  end
+
+  example "failed to sign in with uncertified email" do
+    email = "example@email.com"
+    post "/auth/member/sign_in",
+    params:{email: email, password:"Example123"}
+    expect(response).to have_http_status(401)
+    expect(JSON.parse(response.body)).to eq(
+      JSON.parse(
+        {
+          "error": true,
+          "message": "failed to sign in",
+          "data": "A confirmation email was sent to your account at '#{email}'. You must follow the instructions in the email before your account can be activated"
+        }.to_json
+      )
+    )
   end
 end
