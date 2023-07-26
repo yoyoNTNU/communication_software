574233a515a3a5ed0f76d40035afdef81bb933c3
yoyoNTNU <yoyo102102102@gmail.com>
2023-07-26 17:59:49 +0800

    feat: session unit test with check email deliver

--- backend/spec/requests/auth/sessions_spec.rb
+++ backend/spec/requests/auth/sessions_spec.rb
@@ -8,28 +8,45 @@ end
 
 RSpec.describe "Auth::Sessions", type: :request do
   before do
-    m=Member.new(user_id:"test", name:"test", phone:"0912345678", email:"example@gmail.com", password:"Example123", is_login_mail: false)
+    m=Member.new(user_id:"test1", name:"test", phone:"0912345678", email:"example1@gmail.com", password:"Example123", is_login_mail: true)
+    m.skip_confirmation!
+    m.save
+    m=Member.new(user_id:"test2", name:"test", phone:"0912345679", email:"example2@gmail.com", password:"Example123", is_login_mail: false)
     m.skip_confirmation!
     m.save
   end
 
   describe "POST /auth/member/sign_in" do 
-    example "succeed to sign in" do 
-      post "/auth/member/sign_in",
-      params:{email:"example@gmail.com",password:"Example123"}
+    example "succeed to sign in and sent email" do 
+      post "/auth/member/sign_in",params:{email:"example1@gmail.com",password:"Example123"}
       expect(response).to have_http_status(200)
       parsed_body = JSON.parse(response.body)
-      expect(parsed_body["data"]["user_id"]).to eq("test")
+      expect(parsed_body["data"]["user_id"]).to eq("test1")
       expect(parsed_body["data"]["name"]).to eq("test")
-      expect(parsed_body["data"]["email"]).to eq("example@gmail.com")
+      expect(parsed_body["data"]["email"]).to eq("example1@gmail.com")
       expect(parsed_body["data"]["phone"]).to eq("0912345678")
       expect(parsed_body["error"]).to eq(false) 
       expect(parsed_body["message"]).to eq("succeed to sign in")
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+      expect(ActionMailer::Base.deliveries.first.to).to include("example1@gmail.com")
+      expect(ActionMailer::Base.deliveries.first.subject).to eq("【Express Message】登入成功通知")
+    end
+
+    example "succeed to sign in and does not sent email" do 
+      post "/auth/member/sign_in",params:{email:"example2@gmail.com",password:"Example123"}
+      expect(response).to have_http_status(200)
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body["data"]["user_id"]).to eq("test2")
+      expect(parsed_body["data"]["name"]).to eq("test")
+      expect(parsed_body["data"]["email"]).to eq("example2@gmail.com")
+      expect(parsed_body["data"]["phone"]).to eq("0912345679")
+      expect(parsed_body["error"]).to eq(false) 
+      expect(parsed_body["message"]).to eq("succeed to sign in")
+      expect(ActionMailer::Base.deliveries.count).to eq(0)
     end
     
-    example "failed to sign in with incorrect password" do 
-      post "/auth/member/sign_in",
-      params:{email:"example@gmail.com",password:""}
+    example "failed to sign in with incorrect password and sent email" do 
+      post "/auth/member/sign_in",params:{email:"example1@gmail.com",password:""}
       expect(response).to have_http_status(401)
       expect(JSON.parse(response.body)).to eq(
         JSON.parse(
@@ -40,11 +57,28 @@ RSpec.describe "Auth::Sessions", type: :request do
           }.to_json
         )
       )
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+      expect(ActionMailer::Base.deliveries.first.to).to include("example1@gmail.com")
+      expect(ActionMailer::Base.deliveries.first.subject).to eq("【Express Message】登入失敗通知")
+    end
+
+    example "failed to sign in with incorrect password and does not sent email" do 
+      post "/auth/member/sign_in",params:{email:"example2@gmail.com",password:""}
+      expect(response).to have_http_status(401)
+      expect(JSON.parse(response.body)).to eq(
+        JSON.parse(
+          {
+            "error": true,
+            "message": "failed to sign in",
+            "data": "Invalid login credentials. Please try again."
+          }.to_json
+        )
+      )
+      expect(ActionMailer::Base.deliveries.count).to eq(0)
     end
 
     example "failed to sign in with incorrect email" do 
-      post "/auth/member/sign_in",
-      params:{email:"",password:"Example123"}
+      post "/auth/member/sign_in",params:{email:"",password:"Example123"}
       expect(response).to have_http_status(401)
       expect(JSON.parse(response.body)).to eq(
         JSON.parse(
@@ -55,34 +89,33 @@ RSpec.describe "Auth::Sessions", type: :request do
           }.to_json
         )
       )
+      expect(ActionMailer::Base.deliveries.count).to eq(0)
     end
-  end
-  
-  describe "POST /auth/member/sign_in" do
-    example "failed to sign in with uncertified email" do
-      m=Member.new(user_id:"test1",name:"test1",phone:"09123456789",email:"example1@gmail.com",password:"Example124", is_login_mail: false)
+
+    example "failed to sign in with unconfirmed email" do
+      m=Member.new(user_id:"new",name:"new",phone:"0900000000",email:"example3@gmail.com",password:"Example123", is_login_mail: true)
       m.save
       post "/auth/member/sign_in",
-      params:{email: "example1@gmail.com", password:"Example124"}
+      params:{email: "example3@gmail.com", password:"Example123"}
       expect(response).to have_http_status(401)
       expect(JSON.parse(response.body)).to eq(
         JSON.parse(
           {
             "error": true,
             "message": "failed to sign in",
-            "data": "A confirmation email was sent to your account at 'example1@gmail.com'. You must follow the instructions in the email before your account can be activated"
+            "data": "A confirmation email was sent to your account at 'example3@gmail.com'. You must follow the instructions in the email before your account can be activated"
           }.to_json
         )
       )
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+      expect(ActionMailer::Base.deliveries.first.to).to include("example3@gmail.com")
+      expect(ActionMailer::Base.deliveries.first.subject).to eq("【Express Message】登入失敗通知")
     end
   end
 
   describe "DELETE /auth/member/sign_out" do
     example "succeed to sign out" do
-      m=Member.new(user_id:"test",name:"test",phone:"0912345678",email:"example@gmail.com",password:"Example123")
-      m.skip_confirmation!
-      m.save
-      post "/auth/member/sign_in",params:{email:"example@gmail.com",password:"Example123"}
+      post "/auth/member/sign_in",params:{email:"example1@gmail.com",password:"Example123"}
       @header={Authorization: response.headers["Authorization"]} 
       delete "/auth/member/sign_out", headers:@header
       expect(response).to have_http_status(200)
@@ -98,10 +131,7 @@ RSpec.describe "Auth::Sessions", type: :request do
     end
 
     example "failed to sign out" do
-      m=Member.new(user_id:"test",name:"test",phone:"0912345678",email:"example@gmail.com",password:"Example123")
-      m.skip_confirmation!
-      m.save
-      post "/auth/member/sign_in",params:{email:"example@gmail.com",password:"Example123"}
+      post "/auth/member/sign_in",params:{email:"example1@gmail.com",password:"Example123"}
       @header={Authorization: response.headers["Authorization"]} 
       delete "/auth/member/sign_out", headers:@header
       delete "/auth/member/sign_out", headers:@header
