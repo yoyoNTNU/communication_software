07cfc285887f1ce5a7c7314e7080bbfa8f073b3b
weihsiang120 <weihsiang120@gmail.com>
2023-07-27 12:51:34 +0800

    feat: create friendrequest routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  resources :friend_requests
-    mount_devise_token_auth_for 'Member', at: 'auth/member' ,controllers:{
-      confirmations:      'auth/confirmations',
-      registrations:      'auth/registrations',
-      sessions:           'auth/sessions',
-      passwords:          'auth/passwords',
-      token_validations:  'auth/token_validations'
-    }
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    namespace :api do
-      get 'member/info'=> 'member#show'
-      patch 'member/info' => 'member#update'
-
-    end
+  mount_devise_token_auth_for 'Member', at: 'auth/member' ,controllers:{
+    confirmations:      'auth/confirmations',
+    registrations:      'auth/registrations',
+    sessions:           'auth/sessions',
+    passwords:          'auth/passwords',
+    token_validations:  'auth/token_validations'
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    get 'member/info'=> 'member#show'
+    patch 'member/info' => 'member#update'
+    resources :friend_requests
+  end
 end
