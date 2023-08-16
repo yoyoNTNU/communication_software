d8e77f072c02a23ed097dad856bd5259e1063fda
yoyoNTNU <yoyo102102102@gmail.com>
2023-08-16 13:25:59 +0800

    feat:create message controller

--- backend/app/models/message.rb
+++ backend/app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ApplicationRecord
     validate  :check_message_exist
     belongs_to :member
     belongs_to :chatroom
+    belongs_to :reply_to, class_name: 'Message', optional: true
     mount_uploader :photo , MessageUploader
 
     def check_message_exist
