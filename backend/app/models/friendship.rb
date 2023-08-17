7236763f081825808c7842bafd77853c75f94b07
yoyoNTNU <yoyo102102102@gmail.com>
2023-08-17 13:32:20 +0800

    feat:add and remove friendship add or remove chatroom

--- backend/app/models/friendship.rb
+++ backend/app/models/friendship.rb
@@ -1,7 +1,9 @@
 class Friendship < ApplicationRecord
   before_create :set_default_nickname
+  after_create :create_chatroom
   after_create :create_inverse_relationship
   after_destroy :destroy_inverse_relationship
+  after_destroy :destroy_chatroom
 
   belongs_to :member
   belongs_to :friend, class_name: 'Member'
@@ -17,9 +19,24 @@ class Friendship < ApplicationRecord
     friend.friendships.create(friend: member)
   end
 
+  def create_chatroom
+    f=Friendship.find_by(member:friend,friend:member)
+    if f && Chatroom.find_by(type_:"friend",type_id:f.id)
+      c=Chatroom.find_by(type_:"friend",type_id:f.id)
+    else 
+      c=Chatroom.create(type_:"friend",type_id:id)
+    end
+    ChatroomMember.create(member:member,chatroom_id:c.id)
+  end
+
   def destroy_inverse_relationship
     friendship = friend.friendships.find_by(friend: member)
     friendship.destroy if friendship
+  end
+
+  def destroy_chatroom
+    c=Chatroom.find_by(type_:"friend",type_id:id)
+    c.destroy if c
   end
 
   def set_default_nickname
