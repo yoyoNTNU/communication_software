b48adf47bd9757d8bbd29a1ec4822073f76969d9
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-08-22 20:00:19 +0800

    feat[login_screen]: Finish Login Action

--- frontend/lib/login/login_screen.dart
+++ frontend/lib/login/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:express_message/style.dart';
 import 'package:express_message/login/login_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:express_message/login/bloc/login_bloc.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -27,45 +29,94 @@ class _LoginState extends State<Login> {
           child: Column(
             children: [
               const AppLogo(),
-              AppBox(
-                  title: "使用者登入",
-                  content: Column(
-                    children: [
-                      AppTextField(
-                          key: UniqueKey(),
-                          controller: _accountController,
-                          labelText: '帳號（電子信箱）',
-                          hintText: '請輸入帳號',
-                          errorText: null),
-                      const SizedBox(height: 16),
-                      AppTextField(
-                          key: UniqueKey(),
-                          controller: _passwordController,
-                          labelText: '密碼',
-                          hintText: '請輸入密碼',
-                          errorText: null,
-                          isPassword: true),
-                      const SizedBox(height: 16),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          // Buttons
-                          ElevatedButton(
-                            onPressed: null,
-                            style: AppStyle.primaryBtn(),
-                            child: const Text("登入"),
-                          ),
-                          const SizedBox(width: 16),
-                          // Outline Button
-                          OutlinedButton(
-                            onPressed: null,
-                            style: AppStyle.primaryBtn(),
-                            child: const Text("忘記密碼"),
-                          ),
-                        ],
-                      ),
-                    ],
-                  )),
+              BlocProvider(
+                create: (context) => LoginBloc(),
+                child: AppBox(
+                    title: "使用者登入",
+                    content: BlocBuilder<LoginBloc, LoginState>(
+                      builder: (context, state) {
+                        return Column(
+                          children: [
+                            AppTextField(
+                              key: UniqueKey(),
+                              controller: _accountController,
+                              labelText: '帳號（電子信箱）',
+                              hintText: '請輸入帳號',
+                              errorText:
+                                  state is LoginFailure ? state.error : null,
+                              onChanged: (value) {
+                                context.read<LoginBloc>().add(
+                                      LoginTextFieldChanged(),
+                                    );
+                              },
+                            ),
+                            const SizedBox(height: 16),
+                            AppTextField(
+                              key: UniqueKey(),
+                              controller: _passwordController,
+                              isPassword: true,
+                              labelText: '密碼',
+                              hintText: '請輸入密碼',
+                              errorText:
+                                  state is LoginFailure ? state.error : null,
+                              onChanged: (value) {
+                                context.read<LoginBloc>().add(
+                                      LoginTextFieldChanged(),
+                                    );
+                              },
+                            ),
+                            const SizedBox(height: 16),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                // Buttons
+                                BlocBuilder<LoginBloc, LoginState>(
+                                  builder: (context, state) {
+                                    return ElevatedButton(
+                                      onPressed: () {
+                                        context.read<LoginBloc>().add(
+                                              LoginButtonPressed(
+                                                account:
+                                                    _accountController.text,
+                                                password:
+                                                    _passwordController.text,
+                                              ),
+                                            );
+                                      },
+                                      style: AppStyle.primaryBtn(),
+                                      child: Row(
+                                        children: [
+                                          if (state is LoginLoading)
+                                            const SizedBox(
+                                              width: 16,
+                                              height: 16,
+                                              child: CircularProgressIndicator(
+                                                strokeWidth: 2,
+                                                color: AppStyle.white,
+                                              ),
+                                            ),
+                                          if (state is LoginLoading)
+                                            const SizedBox(width: 8),
+                                          const Text("登入"),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                ),
+                                const SizedBox(width: 16),
+                                // Outline Button
+                                OutlinedButton(
+                                  onPressed: null,
+                                  style: AppStyle.primaryBtn(),
+                                  child: const Text("忘記密碼"),
+                                ),
+                              ],
+                            ),
+                          ],
+                        );
+                      },
+                    )),
+              ),
               const SizedBox(height: 32),
               const ElevatedButton(
                 onPressed: null,
