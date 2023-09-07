bc71ee90aa0c770f0f7ef08e5d99e67dc0aec8e7
yoyoNTNU <yoyo102102102@gmail.com>
2023-09-07 12:42:18 +0800

    feat: new widget for sign up fail UI

--- frontend/lib/signup/signup_widget.dart
+++ frontend/lib/signup/signup_widget.dart
@@ -288,3 +288,113 @@ class _AppTextFieldState extends State<AppTextField> {
     );
   }
 }
+
+class ErrorShow extends StatefulWidget {
+  final List<String> mainType;
+  final List<String> mainIcon;
+  final List<List<String>> secType;
+  final List<List<bool>> isPass;
+
+  const ErrorShow({
+    super.key,
+    required this.mainType,
+    required this.mainIcon,
+    required this.secType,
+    required this.isPass,
+  });
+
+  @override
+  State<ErrorShow> createState() => _ErrorShowState();
+}
+
+class _ErrorShowState extends State<ErrorShow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: AppStyle.white,
+          borderRadius: BorderRadius.circular(8.0),
+          border: Border.all(
+            color: AppStyle.gray,
+            width: 2.0,
+          ),
+        ),
+        child: Column(
+          children: List.generate(4, (index1) {
+            return Column(children: [
+              MainLine(
+                  icon: widget.mainIcon[index1], type: widget.mainType[index1]),
+              Column(
+                  children:
+                      List.generate(widget.secType[index1].length, (index2) {
+                return SecLine(
+                    isPass: widget.isPass[index1][index2],
+                    type: widget.secType[index1][index2]);
+              }))
+            ]);
+          }),
+        ));
+  }
+}
+
+class MainLine extends StatelessWidget {
+  final String icon;
+  final String type;
+
+  const MainLine({super.key, required this.icon, required this.type});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(
+          icon,
+          width: 24,
+          height: 24,
+        ),
+        const SizedBox(width: 8),
+        Text(
+          type,
+          style: AppStyle.caption(),
+        )
+      ],
+    );
+  }
+}
+
+class SecLine extends StatelessWidget {
+  final bool isPass;
+  final String type;
+
+  const SecLine({super.key, required this.isPass, required this.type});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      const SizedBox(
+        width: 24,
+        height: 24,
+      ),
+      isPass
+          ? Image.asset(
+              'assets/icons/pass.png',
+              width: 24,
+              height: 24,
+            )
+          : Image.asset(
+              'assets/icons/not_pass.png',
+              width: 24,
+              height: 24,
+            ),
+      const SizedBox(width: 8),
+      Text(
+        type,
+        textAlign: TextAlign.left,
+        style: AppStyle.caption(
+            color: isPass ? Colors.lightGreenAccent[400]! : AppStyle.red),
+      ),
+    ]);
+  }
+}
