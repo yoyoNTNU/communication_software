c88bebb31d4673a9be2c88b8adaafe818d855fb7
yoyoNTNU <yoyo102102102@gmail.com>
2023-09-10 18:21:06 +0800

    chore: move some function from main to main dependency

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,54 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:proj/signup/signup_screen.dart';
-import './materials/navbar.dart';
+import 'package:proj/materials/navbar.dart';
 import 'package:proj/login/login_screen.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:proj/data.dart';
-import 'package:http/http.dart' as http;
+import 'package:proj/main_dependency.dart';
 
 // Define http host name
 const String host = 'express-message-development.onrender.com';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // 确保 WidgetsFlutterBinding 已初始化
+  WidgetsFlutterBinding.ensureInitialized();
 
-  // 初始化数据库和获取令牌
   final token = await initDatabaseAndGetToken();
 
-  // 验证令牌是否有效
   final isTokenValid = await verifyToken(token);
 
   runApp(ChatApp(isTokenValid: isTokenValid));
-}
-
-Future<String?> initDatabaseAndGetToken() async {
-  // 初始化数据库
-  databaseFactoryOrNull = null;
-  databaseFactory = databaseFactoryFfi;
-  sqfliteFfiInit();
-
-  // 从数据库中获取令牌
-  final token = await DatabaseHelper.instance.getToken();
-  return token?.authorization;
-}
-
-Future<bool> verifyToken(String? token) async {
-  if (token == null) {
-    return false; // 令牌不存在，无效
-  }
-
-  // 调用 API 验证令牌
-  final response = await http.get(
-    Uri(scheme: 'https', host: host, path: '/api/member/info'),
-    headers: {'Authorization': token},
-  );
-
-  if (response.statusCode == 200) {
-    return true; // 令牌有效
-  } else {
-    return false; // 令牌无效
-  }
 }
 
 class ChatApp extends StatelessWidget {
@@ -58,7 +24,6 @@ class ChatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 根据令牌验证结果导航到不同的路径
     final initialRoute = isTokenValid ? '/home' : '/login';
 
     return MaterialApp(
