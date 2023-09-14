1fd2fb95729ee651591c5d58a8cba28bf836fd07
yoyoNTNU <yoyo102102102@gmail.com>
2023-09-14 13:04:30 +0800

    fix; for mobile device

--- frontend/lib/sent_reset_email/sent_reset_email_api.dart
+++ frontend/lib/sent_reset_email/sent_reset_email_api.dart
@@ -5,7 +5,7 @@ class ResetEmailAPI {
   static Future<int> sentResetEmail(String email) async {
     final Map<String, String> body_ = {
       'email': email,
-      'redirect_url': '/auth/member/password/reset'
+      'redirect_url': 'auth/member/password/reset'
     };
     final response = await http.post(
         Uri(scheme: 'https', host: host, path: '/auth/member/password'),
