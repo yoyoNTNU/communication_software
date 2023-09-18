de7b456f5600019accce568f582f60e0c8b43594
yoyoNTNU <yoyo102102102@gmail.com>
2023-09-18 10:16:13 +0800

    temp

--- frontend/lib/homepage/homepage_api.dart
+++ frontend/lib/homepage/homepage_api.dart
@@ -4,9 +4,20 @@ import 'package:proj/main.dart';
 import 'package:proj/data.dart';
 
 class GetInfoAPI {
+  static String? token;
+  static bool isTokenInitialized = false;
+
+  static Future<void> initializeToken() async {
+    if (!isTokenInitialized) {
+      final dbToken = await DatabaseHelper.instance.getToken();
+      token = dbToken?.authorization;
+      isTokenInitialized = true;
+      print("init");
+    }
+  }
+
   static Future<Map<String, dynamic>> getInfo() async {
-    final dbToken = await DatabaseHelper.instance.getToken();
-    final token = dbToken?.authorization;
+    await initializeToken();
     final response = await http.get(
       Uri(scheme: 'https', host: host, path: '/api/member/info'),
       headers: {'Authorization': token ?? ""},
@@ -25,6 +36,33 @@ class GetInfoAPI {
       };
 
       return info;
+    } else {
+      throw Exception('API request failed with status ${response.statusCode}');
+    }
+  }
+
+  static Future<List<Map<String, dynamic>>> getFriend() async {
+    await initializeToken();
+    dynamic response = await http.get(
+      Uri(scheme: 'https', host: host, path: '/api/friends'),
+      headers: {'Authorization': token ?? ""},
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> responseData = json.decode(response.body);
+      List<dynamic> infoData = responseData['data'];
+      List<Map<String, dynamic>> friendsID = infoData.map((friend) {
+        return {
+          "id": friend['friend_id'],
+          "nickname": friend['nickname'],
+        };
+      }).toList();
+
+      response = await http.get(
+        Uri(scheme: 'https', host: host, path: '/api/friends'),
+        headers: {'Authorization': token ?? ""},
+      );
+
+      return friendsID;
     } else {
       throw Exception('API request failed with status ${response.statusCode}');
     }
