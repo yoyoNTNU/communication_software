8699f18a0225a3541d800c237f10eae3742954d4
yoyoNTNU <yoyo102102102@gmail.com>
2023-09-19 14:01:19 +0800

    feat: upload file to google cloud storage

---
+++ backend/config/initializers/carrierwave.rb
@@ -0,0 +1,9 @@
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_credentials = {
+    provider:"Google",
+    google_project: ENV['GOOGLE_PROJECT_ID'],
+    google_json_key_location: ENV['GOOGLE_CLOUD_KEYFILE_PATH']
+  }
+  config.fog_directory = 'express_message_uploader'
+end
