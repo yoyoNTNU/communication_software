38f6dd3d14f5e5a31afdf395ef3687c6882b384f
yoyoNTNU <yoyo102102102@gmail.com>
2023-10-11 09:47:28 +0800

    fix:empty file and widget name

--- frontend/lib/group/widget/title.dart
+++ frontend/lib/group/widget/title.dart
@@ -1,20 +1,12 @@
 part of 'group_widget.dart';
 
-class Title extends StatelessWidget {
-  final String text;
-
-  // Constructor to receive the text
-  const Title(this.text);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Text(
-        text,
-        style: AppStyle.header(color: AppStyle.gray[700]!),
-        textAlign: TextAlign.left,
-      ),
-    );
-  }
+Widget title(String text) {
+  return SizedBox(
+    width: double.infinity,
+    child: Text(
+      text,
+      style: AppStyle.header(color: AppStyle.gray[700]!),
+      textAlign: TextAlign.left,
+    ),
+  );
 }
