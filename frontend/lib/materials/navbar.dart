f0f2c28bfdbf241910a379b0b3c8232d9a228fb7
yoyoNTNU <yoyo102102102@gmail.com>
2023-10-26 16:56:24 +0800

    fix: add thin line on bottomNavigationBar

--- frontend/lib/materials/navbar.dart
+++ frontend/lib/materials/navbar.dart
@@ -34,99 +34,108 @@ class _NavBarState extends State<NavBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        backgroundColor: AppStyle.white,
-        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-        height: 64,
-        indicatorColor: Colors.transparent,
-        onDestinationSelected: (int index) async {
-          await DatabaseHelper.instance.setHomepageIndex(index);
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        selectedIndex: currentPageIndex,
-        destinations: <Widget>[
-          NavigationDestination(
-            icon: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 8,
-                ),
-                Image.asset("assets/icons/Table_gray.png"),
-                const SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  "主頁",
-                  style: AppStyle.caption(
-                    level: 2,
-                    color: AppStyle.gray[600]!,
-                  ),
-                )
-              ],
-            ),
-            selectedIcon: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 8,
-                ),
-                Image.asset("assets/icons/Table_blue.png"),
-                const SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  "主頁",
-                  style: AppStyle.caption(level: 2, color: AppStyle.blue),
-                )
-              ],
-            ),
-            label: '主頁',
-            tooltip: '',
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(color: AppStyle.blue[50]!),
           ),
-          NavigationDestination(
-            icon: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 8,
-                ),
-                Image.asset("assets/icons/Chat_gray.png"),
-                const SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  "聊天室",
-                  style: AppStyle.caption(
+        ),
+        child: NavigationBar(
+          backgroundColor: AppStyle.white,
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+          height: 64,
+          indicatorColor: Colors.transparent,
+          onDestinationSelected: (int index) async {
+            await DatabaseHelper.instance.setHomepageIndex(index);
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          selectedIndex: currentPageIndex,
+          destinations: <Widget>[
+            NavigationDestination(
+              icon: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Image.asset("assets/icons/Table_gray.png"),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    "主頁",
+                    style: AppStyle.caption(
                       level: 2,
                       color: AppStyle.gray[600]!,
-                      weight: FontWeight.w700),
-                )
-              ],
+                    ),
+                  )
+                ],
+              ),
+              selectedIcon: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Image.asset("assets/icons/Table_blue.png"),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    "主頁",
+                    style: AppStyle.caption(level: 2, color: AppStyle.blue),
+                  )
+                ],
+              ),
+              label: '主頁',
+              tooltip: '',
             ),
-            selectedIcon: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 8,
-                ),
-                Image.asset("assets/icons/Chat_blue.png"),
-                const SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  "聊天室",
-                  style: AppStyle.caption(
-                      level: 2, color: AppStyle.blue, weight: FontWeight.w700),
-                )
-              ],
+            NavigationDestination(
+              icon: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Image.asset("assets/icons/Chat_gray.png"),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    "聊天室",
+                    style: AppStyle.caption(
+                        level: 2,
+                        color: AppStyle.gray[600]!,
+                        weight: FontWeight.w700),
+                  )
+                ],
+              ),
+              selectedIcon: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Image.asset("assets/icons/Chat_blue.png"),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    "聊天室",
+                    style: AppStyle.caption(
+                        level: 2,
+                        color: AppStyle.blue,
+                        weight: FontWeight.w700),
+                  )
+                ],
+              ),
+              label: '聊天室',
+              tooltip: '',
             ),
-            label: '聊天室',
-            tooltip: '',
-          ),
-        ],
+          ],
+        ),
       ),
       body: <Widget>[
         const HomePage(),
