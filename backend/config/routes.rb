cb456549d885fe87847b7fc993c2c10798c00c49
yoyoNTNU <yoyo102102102@gmail.com>
2023-11-02 15:35:49 +0800

    feat: API for transfer chatroomID and typeID

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     get 'auth/member/password/reset'=> 'auth/passwords#reset'
     get 'auth/member/password/final'=> 'auth/passwords#final', as: 'reset_final'
   end
+  get 'ChatroomIDToTypeID' => 'helper#from_chatroomID_to_typeID'
+  get 'TypeIDToChatroomID' => 'helper#from_typeID_to_chatroomID'
   namespace :api do
     get 'member/info'=> 'member#show'
     get 'member/:id/info'=> 'member#other'
